abad3a17a0865b93f596c189d7ab3aa97c424e1f
unique-anjali <94821248+unique-anjali@users.noreply.github.com>
2021-12-02 23:44:41 +0530

    creating code

---
+++ 01.c
@@ -0,0 +1,11 @@
+//w.a.p to check whether a given no. is +ve or non +ve.
+main()
+{
+    int a;
+    printf("enter a number:");
+    scanf("%d",&a);
+    if(a>0)
+    printf("the given number is positive.");
+    else
+    printf("The given number is non-positive.");
+}

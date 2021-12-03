9c015f63ac17d17bfd5c6cbd885de8ccaf17cc7c
@ANJALI CHHETRI <94821248+unique-anjali@users.noreply.github.com>
2021-12-03 14:09:14 +0530

    code
    control instruction

---
+++ 03.c
@@ -0,0 +1,11 @@
+//w.a.p. to check whether a given number is even or odd
+main()
+{
+	int n;
+	printf("enter a number:");
+	scanf("%d",&n);
+	if(n%==0)
+	printf("given number is even .");
+	else
+	printf("given number is odd.");
+}

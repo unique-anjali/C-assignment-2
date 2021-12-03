44fa8553bc69a9ba9ad012ad14c2da2d1462d8c4
@ANJALI CHHETRI <94821248+unique-anjali@users.noreply.github.com>
2021-12-03 14:04:12 +0530

    code
    control instruction

---
+++ 02.c
@@ -0,0 +1,12 @@
+//w.a.p to check a given no. is divisible by 5 or not.
+main()
+{
+    int a;
+    printf("enter a number:");
+    scanf("%d",&a);
+    if(a%5==0)
+        printf("given number is divisible by 5");
+    else
+        printf("given number is not divisible by 5");
+
+}
